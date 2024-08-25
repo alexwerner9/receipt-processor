7ccfea1e717f211c3be36dec9b4ed081a47a7fc5
Alex <alexwerner9@gmail.com>
2024-08-25 01:52:28 -0400

    implement rules

--- app/rules.py
+++ app/rules.py
@@ -1,4 +1,5 @@
 
+import math
 from typing import Dict
 
 
@@ -9,44 +10,74 @@ class BaseRule:
     
 
 class AlphaNum(BaseRule):
+    """Points = number of alphanumeric characters in retailer name."""
     @staticmethod
     def calculate(receipt: Dict):
-        return 0
+        retailer_name = receipt.get('retailer', '')
+        return len([char for char in retailer_name if char.isalnum()])
 
 
 class RoundTotal(BaseRule):
+    """Points = 50 if total is a round dollar amount, 0 otherwise."""
     @staticmethod
     def calculate(receipt: Dict):
+        total = float(receipt.get('total', 0))
+        if total == int(total):
+            return 50
         return 0
     
 
 class QuarterMultiple(BaseRule):
+    """Points = 25 if total is a multiple of 25."""
     @staticmethod
     def calculate(receipt: Dict):
-        return 0
+        total = float(receipt.get('total', 0))
+        if total % 0.25:
+            return 0
+        return 25
 
 
 class ItemPairs(BaseRule):
+    """Points = 5 points for every two items on receipt."""
     @staticmethod
     def calculate(receipt: Dict):
-        return 0
+        items = receipt.get('items', [])
+        num_items = len(items)
+        return 5 * (num_items // 2)
 
 
 class TrimmedLen(BaseRule):
+    """Points = If the trimmed length of the item description is a multiple of 3, 
+    multiply the price by 0.2 and round up to the nearest integer."""
     @staticmethod
     def calculate(receipt: Dict):
-        return 0
+        points = 0
+        for item in receipt.get('items'):
+            description = item.get('shortDescription').strip()
+            price = float(item.get('price'))
+            if not len(description) % 3:
+                price *= 0.2
+                points += math.ceil(price)
+        return points
 
 
 class OddDate(BaseRule):
+    """Points = 6 points if the day in the purchase date is odd."""
     @staticmethod
     def calculate(receipt: Dict):
+        purchase_date = receipt.get('purchaseDate', '0-0-0')
+        date = int(purchase_date.split('-')[2])
+        if date % 2:
+            return 6
         return 0
 
 
 class TwoAndFour(BaseRule):
     @staticmethod
     def calculate(receipt: Dict):
+        purchase_time = receipt.get('purchaseTime', '00:00')
+        if purchase_time > '14:00' and purchase_time < '16:00':
+            return 10
         return 0
     
 
